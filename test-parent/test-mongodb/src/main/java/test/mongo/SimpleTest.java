763db71f9fb0ee865ff0c58fa30e27af5d78918b
cherish2015 <cherish1896@126.com>
2015-04-26 10:26:52 +0800

    add spring-data jar

--- test-parent/test-mongodb/src/main/java/test/mongo/SimpleTest.java
+++ test-parent/test-mongodb/src/main/java/test/mongo/SimpleTest.java
@@ -1,8 +1,14 @@
 package test.mongo;
 
 import java.net.UnknownHostException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 
 public class SimpleTest {
@@ -19,6 +25,19 @@ public class SimpleTest {
 			List<String> dbs = mg.getDatabaseNames();
 			for (int i = 0; i < dbs.size(); i++) {
 				System.err.println(dbs.get(i));
+			}
+			DB db = mg.getDB("mymongo");
+			DBCollection colleciton = db.getCollection("colors");
+			DBCursor dbCursor = colleciton.find();
+			while(dbCursor.hasNext()){
+				DBObject obj = dbCursor.next();
+				Set<String> keys = obj.keySet();
+				System.err.println(keys);
+				Iterator<String> it = keys.iterator();
+				while(it.hasNext()){
+					String key = it.next();
+					System.err.println(obj.get(key));
+				}
 			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
